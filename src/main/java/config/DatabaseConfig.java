bf23986a9425c01331da2fada5ff8224adc1ed4e
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-18 16:29:33 -0500

    DB Configuration

---
+++ src/main/java/config/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+public class DatabaseConfig extends Configuration {
+
+    @Valid
+    @NotNull
+    @JsonProperty("database")
+    private DataSourceFactory database = new DataSourceFactory();
+
+    public DataSourceFactory getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(DataSourceFactory database) {
+        this.database = database;
+    }
+}
