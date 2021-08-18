08fb8ce77a0fbec4eb53ae3676759693509f8cf9
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-08-18 16:23:16 -0500

    EmployeeModel

---
+++ src/main/java/model/EmployeeModel.java
@@ -0,0 +1,76 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class EmployeeModel {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "emp_id")
+    private Integer emp_id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "position")
+    private String position;
+
+    public Integer getEmp_id() {
+        return emp_id;
+    }
+
+    public void setEmp_id(Integer emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
